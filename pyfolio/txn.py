1c0c82dd038579911efbac224f731892190922bf
MoneyDouble <348104201@qq.com>
2025-01-08 13:23:26 +0800

    print txn_norm

--- pyfolio/txn.py
+++ pyfolio/txn.py
@@ -99,10 +99,11 @@ def get_txn_vol(transactions):
     """
 
     txn_norm = transactions.copy()
+    print('txn_norm: ', txn_norm.head())
     txn_norm.index = txn_norm.index.normalize()
         # 确保 'amount' 列存在
-    #if 'amount' not in txn_norm.columns:
-    #    raise ValueError("transactions中缺少'amount'列")
+    if 'amount' not in txn_norm.columns:
+        raise ValueError("transactions中缺少'amount'列")
     txn_norm['abs_amount'] = txn_norm['amount'].apply(lambda x: abs(x))
     #amounts = txn_norm['abs_amount']
     #print('amounts: ', amounts)
