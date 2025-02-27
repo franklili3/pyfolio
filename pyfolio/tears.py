439a1e290713561380ca6d9866db8aae6d797b43
MoneyDouble <348104201@qq.com>
2025-02-27 11:07:59 +0800

    comment print

--- pyfolio/tears.py
+++ pyfolio/tears.py
@@ -829,15 +829,15 @@ def create_round_trip_tear_sheet(returns, positions, transactions,
 
     transactions_closed = round_trips.add_closing_transactions(positions,
                                                                transactions)
-    print('transactions_closed: ', transactions_closed.head())
+    #print('transactions_closed: ', transactions_closed.head())
     portfolio_value = positions.sum(axis='columns') / (1 + returns)
-    print('portfolio_value: ', portfolio_value.head())
+    #print('portfolio_value: ', portfolio_value.head())
     # extract_round_trips requires BoD portfolio_value
     trades = round_trips.extract_round_trips(
         transactions_closed,
         portfolio_value=portfolio_value
     )
-    print('trades: ', trades.head())
+    #print('trades: ', trades.head())
     if len(trades) < 5:
         warnings.warn(
             """Fewer than 5 round-trip trades made.
